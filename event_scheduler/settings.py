b578dd97ce349d47271e5debe501145fc3d62b4b
Ateeb <ateebaijaz15@gmail.com>
2025-05-22 17:17:33 +0530

    FEAT: authentication done regsiter,login,logout

--- event_scheduler/settings.py
+++ event_scheduler/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events',
+    'users',
     'rest_framework',
+    'rest_framework_simplejwt',
     'rest_framework.authtoken',
 ]
 

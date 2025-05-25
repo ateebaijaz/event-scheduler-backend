247ef75c39400908bbf91dfe68e787e595950f6e
Ateeb <ateebaijaz15@gmail.com>
2025-05-25 11:42:41 +0530

    FIX: cors error

--- event_scheduler/settings.py
+++ event_scheduler/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'simple_history',
     'drf_yasg',
+    'corsheaders'
 ]
 
 REST_FRAMEWORK = {
@@ -81,6 +82,7 @@ SWAGGER_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -93,6 +95,10 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://event-scheduler-backend-production.up.railway.app",  # ymy railway
+    "http://localhost:8000",  # for local testing
+]
 ROOT_URLCONF = 'event_scheduler.urls'
 
 TEMPLATES = [
